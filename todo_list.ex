b7423ba5e1ac8809022b42b6997b8e40f1f4cc92
Rijan-Budds <buddsrijan@gmail.com>
2023-08-15 11:18:26 +0545

    todo list

--- todo_list.ex
+++ todo_list.ex
@@ -14,7 +14,3 @@ defmodule TodoList do
       Map.get(todo_list, date, [])
   end
 end
-#c("todo_list.ex")
-#ctodo_list = TodoList.new()
-#todo_list = TodoList.add_entry(todo_list, "2080-3-19", "watch oppenheimer") for adding to list
-#entries_2080_3_19 = TodoList.entries(todo_list, "2080-3-19")
